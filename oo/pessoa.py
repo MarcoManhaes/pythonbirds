45ae6f518f7a05e51b020efd98441fe62694ce86
Marco Manhães <marco.manhaes@gmail.com>
2021-12-11 13:39:34 -0300

    criados atributo de instância complexo

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,13 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f"Olá {id(self)}"
 
 if __name__ == '__main__':
-    p = Pessoa('Júnior')
-    print(p.cumprimentar())
-    print(p.nome)
-    print(p.idade)
+    Junior = Pessoa(nome='Júnior', idade=39)
+    Dilma = Pessoa(Junior, nome='Dilma', idade=60)
+
+    print(Dilma.cumprimentar())
+    print(Dilma.nome)
+    print(Dilma.idade)
+    for filho in Dilma.filhos:
+        print(filho.nome)
+        print(filho.idade)
